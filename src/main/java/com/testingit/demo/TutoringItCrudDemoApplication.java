3cc38bb508fa0d27c19ee2d03655537faa5a5350
Valentin Vucea <valentin@itworksdc.com>
2023-01-26 18:38:38 -0500

    Initial commit

---
+++ src/main/java/com/testingit/demo/TutoringItCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.testingit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringItCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringItCrudDemoApplication.class, args);
+	}
+
+}
