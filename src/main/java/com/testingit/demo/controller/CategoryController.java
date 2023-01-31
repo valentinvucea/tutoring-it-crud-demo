7c9dc427e0fb2fd33a867fd177815e7b0736b766
Valentin Vucea <valentin@itworksdc.com>
2023-01-31 12:53:39 -0500

    Added structure for /categories page (WIP)

--- src/main/java/com/testingit/demo/controller/CategoryController.java
+++ src/main/java/com/testingit/demo/controller/CategoryController.java
@@ -1,8 +1,15 @@
 package com.testingit.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CategoryController {
-
+	// Home page: GET /
+	@RequestMapping("/categories")
+    public String showHomePage(Model model) {
+		model.addAttribute("message", "This is the categories page");
+        return "views/categories/index";
+    }
 }
